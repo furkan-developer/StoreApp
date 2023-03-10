76f887bf5c6f0d9359149a5bd3827cf7903dd0f1
Furkan Aydin <furknn2112@gmail.com>
2023-03-10 15:54:23 +0300

    ADD: Book table in context

--- WebAPI/Models/Repository/RepositoryContext.cs
+++ WebAPI/Models/Repository/RepositoryContext.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAPI.Models.Repository
 {
-    public class RepositoryContext
+    public class RepositoryContext: DbContext
     {
-        
+        public DbSet<Book> Books { get; set; }
     }
 }
