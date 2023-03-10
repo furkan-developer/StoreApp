691fce34aa0bd0b67c5798b258598c3dd3a90dfc
Furkan Aydin <furknn2112@gmail.com>
2023-03-10 16:22:51 +0300

    CREATE: Context connection for SQL Server

--- WebAPI/Models/Repository/RepositoryContext.cs
+++ WebAPI/Models/Repository/RepositoryContext.cs
@@ -8,6 +8,11 @@ namespace WebAPI.Models.Repository
 {
     public class RepositoryContext: DbContext
     {
+        public RepositoryContext(DbContextOptions options)
+        :base(options)
+        {
+            
+        }
         public DbSet<Book> Books { get; set; }
     }
 }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<RepositoryContext>((options)=>{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
