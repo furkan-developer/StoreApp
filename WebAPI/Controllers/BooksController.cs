5bb453dd82802c3b39cf94f009c7bedc7b591c59
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-03-11 13:35:53 +0300

    CREATE: BookController with GetAllBooks action

---
+++ WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Models.Repository;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks(){
+            return Ok(_context.Books.ToList());
+        }
+    }
+}
