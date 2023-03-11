152e26f85f9e4980ad232ecd7f687f67a6f5f951
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-03-11 13:33:41 +0300

    CREATE: Migrations for seed data
    execute,
     # dotnet ef migrations add seedData
     # dotnet ef database update

---
+++ WebAPI/Migrations/20230311095846_seedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, 45.99m, "ASP.NET Core with Web API" },
+                    { 2, 58m, "ASP.NET Core with WPF" },
+                    { 3, 48.49m, "ASP.NET Core with Web Application" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
