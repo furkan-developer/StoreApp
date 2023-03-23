c2d6dc24b84017e2f8d9fa83a7be55d5b2c535ae
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-03-23 15:42:38 +0300

    CREATE_PROJECT: Entities layer

--- Entities/Book.cs
+++ Entities/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Contract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    public class Book
+    public class Book:IEntity
     {
         public int Id { get; set; }
         public String Title { get; set; }
