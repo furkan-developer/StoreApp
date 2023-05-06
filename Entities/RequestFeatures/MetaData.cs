943cbea8141ee5bf5272f7092ab30edb6a89866d
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-05-06 15:45:56 +0300

    CREATE: PagenedList object

---
+++ Entities/RequestFeatures/MetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+    public class MetaData
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotolSize { get; set; }
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPage;
+    }
+}
---
+++ Entities/RequestFeatures/PagedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+    public class PagedList<T>: List<T>
+    {
+        public MetaData MetaData { get; set; }
+        public PagedList(List<T> data, int count, int pageNumber,int pageSize)
+        {
+            MetaData = new MetaData()
+            {
+                CurrentPage = pageNumber,
+                PageSize = pageSize,
+                TotalPage = (int)Math.Ceiling(count / (double)pageSize),
+                TotolSize = count
+            };
+
+            AddRange(data);
+        }
+
+        public static PagedList<T> ToPagedList(IEnumerable<T> data,int pageNumber, int pageSize)
+        {
+            int count = data.Count();
+            var currentPageData = data
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedList<T>(currentPageData, count, pageNumber, pageSize);
+        }
+    }
+}
